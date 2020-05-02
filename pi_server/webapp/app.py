a417832b9d09717356dfa33ddd9c52c9a7b6f1ea
Pedro Villanueva <pedvide@gmail.com>
2020-05-02 16:04:17 +0200

    linting and formatting

--- pi_server/webapp/app.py
+++ pi_server/webapp/app.py
@@ -4,22 +4,22 @@ import datetime
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-   now = datetime.datetime.now()
-   timeString = now.strftime("%Y-%m-%d %H:%M")
-   templateData = {
-      'title' : 'HELLO!',
-      'time': timeString
-      }
-   return render_template('index.html', **templateData)
+    now = datetime.datetime.now()
+    timeString = now.strftime("%Y-%m-%d %H:%M")
+    templateData = {"title": "HELLO!", "time": timeString}
+    return render_template("index.html", **templateData)
+
 
 @app.route("/api/upload_data")
 def upload_data():
-   
+    pass
 
-if __name__ == '__main__':
-    app.run(debug=True, port=8080, host='0.0.0.0')
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8080, host="0.0.0.0")
 
 # redirect port 8080 to 80 (only available to root) with:
 # sudo iptables -A PREROUTING -t nat -p tcp --dport 80 -j REDIRECT --to-ports 8080
