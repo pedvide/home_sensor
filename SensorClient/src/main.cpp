be02d51f232646ed521628f7663587310a2062e2
Pedro Villanueva <pedvide@gmail.com>
2020-05-03 16:52:20 +0200

    refactor code to support more sensors

--- SensorClient/src/main.cpp
+++ SensorClient/src/main.cpp
@@ -17,28 +17,29 @@ const char *ssid = STASSID;
 const char *password = STAPSK;
 
 //// Main server
-const char* server = SERVER_HOSTNAME;
-const char* api_endpoint = "/api/upload_data";
+const char *server = SERVER_HOSTNAME;
+const char *api_endpoint = "/api/upload_data";
 const uint8_t port = 80;
 
 //// Time
 Timezone Amsterdam;
 
-//// Sensor
-typedef struct{
+///// Common sensor
+const uint32_t sensor_period_s = 5;
+void measure_sensors();
+Ticker measurement_timer(measure_sensors, sensor_period_s * 1e3, 0, MILLIS);
+// AM2320 Sensor
+const char *sensor_am2320_id = "AM2320";
+typedef struct
+{
   time_t epoch;
   float temperature;
   float humidity;
-} SensorData;
-SensorData sensor_data;
+} SensorAM2320Data;
+SensorAM2320Data sensor_data;
 float temperature, humidity;
-const uint32_t sensor_period_s = 5;
-CircularBuffer<SensorData, 255> sensor_buffer;  // Keep some raw data
+CircularBuffer<SensorAM2320Data, 255> sensor_am2320_buffer; // Keep some raw data
 Adafruit_AM2320 am2320 = Adafruit_AM2320();
-void measure_sensor();
-// Updates readings every x seconds
-Ticker measurement_timer(measure_sensor, sensor_period_s * 1e3, 0, MILLIS);
-
 
 //// Post request
 WiFiClient client;
@@ -48,20 +49,25 @@ String response;
 // String postData;
 // Updates readings every x/2 seconds
 void send_data();
-Ticker post_timer(send_data, int(sensor_period_s/2) * 1e3, 0, MILLIS);
+Ticker send_timer(send_data, int(sensor_period_s / 2) * 1e3, 0, MILLIS);
 
-void connect_to_wifi() {
+////// Setup functions
+
+void connect_to_wifi()
+{
   // Connect to Wi-Fi
   Serial.println("Connecting to WiFi");
   WiFi.begin(ssid, password);
   WiFi.mode(WIFI_STA); //WiFi mode station (connect to wifi router only
-  while (!WiFi.isConnected()) {
+  while (!WiFi.isConnected())
+  {
     delay(1000);
     Serial.print(".");
   }
   Serial.println();
-  
-  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+
+  while (WiFi.waitForConnectResult() != WL_CONNECTED)
+  {
     Serial.println("Fail connecting");
     delay(5000);
     ESP.restart();
@@ -71,59 +77,109 @@ void connect_to_wifi() {
 
   // Print ESP8266 Local IP Address
   Serial.println("Connected!");
-  Serial.print("IP: "); Serial.println(WiFi.localIP());
-  Serial.print("MAC sha1: "); Serial.println(mac_sha);
-  WiFi.setAutoReconnect(true);  
+  Serial.print("IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.print("MAC sha1: ");
+  Serial.println(mac_sha);
+  WiFi.setAutoReconnect(true);
 }
 
-
-void connect_to_time() {
+void connect_to_time()
+{
   setDebug(ezDebugLevel_t::INFO);
   waitForSync();
   setInterval(60 * 60); // 1h in seconds
 
   Serial.println("UTC: " + UTC.dateTime());
-	
-	Amsterdam.setLocation("Europe/Amsterdam");
-	Serial.println("Amsterdam time: " + Amsterdam.dateTime());
+
+  Amsterdam.setLocation("Europe/Amsterdam");
+  Serial.println("Amsterdam time: " + Amsterdam.dateTime());
 }
 
+void setup_sensors()
+{
+  am2320.begin();
+  delay(500);
+}
 
-void measure_sensor(){
+////// Measurement functions
+
+void measure_am2320_sensor()
+{
   // sensor
   humidity = am2320.readHumidity();
-  if (isnan(humidity)) {
+  if (isnan(humidity))
+  {
     Serial.println("Error reading humidity.");
   }
   temperature = am2320.readTemperature();
-  if (isnan(temperature)) {
+  if (isnan(temperature))
+  {
     Serial.println("Error reading temperature.");
   }
-  if (isnan(temperature) && isnan(humidity)) {
+  if (isnan(temperature) && isnan(humidity))
+  {
     // All datapoints are invalid
     return;
   }
-  sensor_buffer.push(SensorData{UTC.now(), temperature, humidity});
+  sensor_am2320_buffer.push(SensorAM2320Data{UTC.now(), temperature, humidity});
   Serial.printf("Sensor values: %.2f C, %.2f %%.\n", temperature, humidity);
 }
 
-void send_data() {
+void measure_sensors()
+{
+  measure_am2320_sensor();
+}
+
+////// Send data functions
+
+bool post_request(String &data)
+{
   // Post Data
-  if (WiFi.status() != WL_CONNECTED) {
+  if (WiFi.status() != WL_CONNECTED)
+  {
     connect_to_wifi();
+    return false;
+  }
+
+  http.begin(client, server, port, api_endpoint);
+  http.addHeader("Content-Type", "application/json");
+
+  httpCode = http.POST(data); //Send the request
+  if (httpCode > 0)
+  {
+    response = http.getString(); //Get the response payload
+    response.trim();
+    response.replace("\n", "");
+    Serial.printf("HTTP code: %d. Response: %s.\n", httpCode, response.c_str());
+    if (httpCode == HTTP_CODE_OK)
+    {
+      http.end();
+      return true;
+    }
+  }
+  else
+  {
+    Serial.printf("HTTP Error code: %d.\n", httpCode); //Print HTTP return code and response
+  }
+  http.end();
+  return false;
+}
+
+void send_am2320_data()
+{
+  if (sensor_am2320_buffer.isEmpty())
+  {
     return;
   }
 
-  if (sensor_buffer.isEmpty()) {
-    return;
-  }
-    
   // Attempt to send data
-  sensor_data = sensor_buffer.last();
+  sensor_data = sensor_am2320_buffer.last();
 
   // Prepare JSON document
   StaticJsonDocument<400> doc;
-  doc["sensor_id"] = mac_sha;
+  doc["station_id"] = mac_sha;
+  doc["sensor_id"] = sensor_am2320_id;
   doc["time"] = sensor_data.epoch;
 
   JsonArray data = doc.createNestedArray("data");
@@ -131,65 +187,54 @@ void send_data() {
   JsonObject data_0 = data.createNestedObject();
   data_0["name"] = "temperature";
   data_0["unit"] = "C";
-  data_0["value"] = isnan(sensor_data.temperature) ? "null" : String(sensor_data.temperature);
+  data_0["value"] = isnan(sensor_data.temperature) ? (char *)0 : String(sensor_data.temperature);
 
   JsonObject data_1 = data.createNestedObject();
   data_1["name"] = "humidity";
   data_1["unit"] = "%";
-  data_1["value"] = isnan(sensor_data.humidity) ? "null" : String(sensor_data.humidity);
+  data_1["value"] = isnan(sensor_data.humidity) ? (char *)0 : String(sensor_data.humidity);
 
   // Serialize JSON document
-  String postData;
-  serializeJson(doc, postData);
+  String post_data;
+  serializeJson(doc, post_data);
 
-  http.begin(client, server, port, api_endpoint);
-  http.addHeader("Content-Type", "application/json");
+  bool success = post_request(post_data);
 
-  // Serial.println("Sending: '" + postData + "' to " + server + api_endpoint + ".");
-  httpCode = http.POST(postData);   //Send the request
-  if (httpCode > 0) {
-    response = http.getString();    //Get the response payload
-    response.trim();
-    response.replace("\n", "");
-    Serial.printf("HTTP code: %d. Response: %s.\n", httpCode, response.c_str());  //Print HTTP return code and response
-    if (httpCode == HTTP_CODE_OK) {
-      // Data was sent, remove the data point
-      sensor_buffer.pop();
-    }
-  } else {
-    Serial.printf("HTTP Error code: %d.\n", httpCode);  //Print HTTP return code and response
+  if (success)
+  {
+    sensor_am2320_buffer.pop();
   }
-  http.end();  //Close connection
-
 }
 
+void send_data()
+{
+  send_am2320_data();
+}
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
 
   connect_to_wifi();
 
   connect_to_time();
 
-  // Sensor
-  am2320.begin();
-  delay(500);
+  setup_sensors();
 
   // Timer
   measurement_timer.start();
-  post_timer.start();
+  send_timer.start();
 }
 
-
-void loop() {
+void loop()
+{
   // Update time if needed
   events();
 
   // Update timer
   measurement_timer.update();
-  post_timer.update();
+  send_timer.update();
 
-  
   // send_data();
 
   // if ((millis() % 5000) == 0) {
@@ -197,10 +242,9 @@ void loop() {
   //   while (!sensor_buffer.isEmpty()) {
   //     SensorData data;
   //     data = sensor_buffer.pop();
-  //     Serial.printf("[%s], T = %.2f C, H = %.1f %%.\n", 
+  //     Serial.printf("[%s], T = %.2f C, H = %.1f %%.\n",
   //       Amsterdam.dateTime(data.epoch).c_str(), data.temperature, data.humidity);
   //   }
   //   Serial.println("End of buffer.");
   // }
-
 }
