449061ec580a064229c5f569914efa385c48c99b
Pedro Villanueva <pedvide@gmail.com>
2020-06-09 19:42:35 +0200

    full test coverage in rest api

--- pi_server/tests/test_all.py
+++ pi_server/tests/test_all.py
@@ -266,6 +266,14 @@ def test_modify_station(client, db_session, station_zero, station_one):
     assert response.json() == modified_station_out
 
 
+def test_modify_wrong_station(client, db_session, station_zero):
+    station0_in, station0_out = station_zero
+
+    # PUT a station that doesn't exist
+    response = client.put("/api/stations/2", json=station0_in)
+    assert response.status_code == 404
+
+
 def test_sensors(client, db_session, station_zero):
     station_in, station_out = station_zero
 
@@ -285,6 +293,29 @@ def test_post_one_measurement(client, db_session, station_zero, measurement_one)
     response = client.post("/api/stations/1/measurements", json=[m0_in])
     assert response.status_code == 201
     assert response.json() == [m0_out]
+
+
+def test_post_measurement_no_station(client, db_session):
+    # POST to a station that doesn't exist
+    response = client.post("/api/stations/1/measurements", json=[])
+    assert response.status_code == 404
+
+
+def test_post_wrong_measurement(client, db_session, station_zero, measurement_one):
+
+    station_in, _ = station_zero
+    m0_in, m0_out = measurement_one
+
+    client.post("/api/stations", json=station_in)
+
+    m0_in["sensor_id"] = 3
+    response = client.post("/api/stations/1/measurements", json=[m0_in])
+    assert response.status_code == 404
+    m0_in["sensor_id"] = 1
+
+    m0_in["magnitude_id"] = 3
+    response = client.post("/api/stations/1/measurements", json=[m0_in])
+    assert response.status_code == 404
 
 
 def test_post_two_measurements(client, db_session, station_zero, measurement_one, measurement_two):
@@ -322,3 +353,9 @@ def test_get_station_measurement(client, db_session, station_zero, measurement_o
     response = client.get("/api/stations/1/measurements")
     assert response.status_code == 200
     assert response.json() == [m0_out]
+
+
+def test_get_measurement_no_station(client, db_session):
+    # POST to a station that doesn't exist
+    response = client.get("/api/stations/1/measurements")
+    assert response.status_code == 404
