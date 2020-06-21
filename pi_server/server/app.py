0fc4d93fe8827113375c5e67c67b7f19358885cc
Pedro Villanueva <pedvide@gmail.com>
2020-06-21 20:26:50 +0200

    put docs inside /api route

--- pi_server/server/app.py
+++ pi_server/server/app.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
-from fastapi.middleware.cors import CORSMiddleware
+
+# from fastapi.middleware.cors import CORSMiddleware
 
 from . import rest_api
 from .database import engine
@@ -8,20 +8,19 @@ from . import models
 
 models.Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
+app = FastAPI(docs_url="/api/docs", redoc_url="/api/redoc")
 app.include_router(rest_api.router, prefix="/api")
-app.mount("/", StaticFiles(directory="client/dist", html=True), name="frontend")
 
-origins = [
-    "http://localhost",
-    "http://localhost:80",
-    "http://localhost:8080",
-]
+# origins = [
+#     "http://localhost",
+#     "http://localhost:80",
+#     "http://localhost:8080",
+# ]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
