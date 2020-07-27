bc5c05673256f1738abfd196d824280f1162c8cf
Pedro Villanueva <pedvide@gmail.com>
2020-07-27 22:08:48 +0200

    allow CORS

--- rest_server/rest_server/app.py
+++ rest_server/rest_server/app.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-# from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 
 from . import rest_api
 from .database import engine
@@ -11,16 +11,14 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI(docs_url="/api/docs", redoc_url="/api/redoc")
 app.include_router(rest_api.router, prefix="/api")
 
-# origins = [
-#     "http://localhost",
-#     "http://localhost:80",
-#     "http://localhost:8080",
-# ]
+origins = [
+    "*",
+]
 
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=origins,
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
