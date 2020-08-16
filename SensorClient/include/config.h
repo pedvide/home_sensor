a712a70d186de172234b29cba94c4a73add12b04
Pedro Villanueva <pedvide@gmail.com>
2020-08-16 19:54:00 +0200

    add template for configs

---
+++ SensorClient/include/config.h
@@ -0,0 +1,8 @@
+#ifndef STASSID
+#define STASSID "WIFI ID"
+#define STAPSK "WIFI PASSWORD"
+#endif
+
+#define SERVER_HOSTNAME "hostname of the rest_server"
+
+#define LOCATION "location of this station, change for each station!"
