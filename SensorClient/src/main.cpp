6a5d78a17ae3679722e44ce705eb55b8bbf14fba
Pedro Villanueva <pedvide@gmail.com>
2021-01-12 21:55:15 +0100

    added HDC1080 sensor

--- SensorClient/src/main.cpp
+++ SensorClient/src/main.cpp
@@ -97,6 +97,17 @@ Ticker ccs811_measurement_timer(measure_ccs811_sensor, ccs811_period_s * 1e3, 0,
                                 MILLIS);
 #endif
 
+#ifdef HAS_HDC1080
+// HAS_HDC1080 Sensor
+uint8_t hdc1080_sensor_id, hdc1080_temp_id, hdc1080_hum_id;
+const char *sensor_hdc1080_name = "HDC080";
+ClosedCube_HDC1080 hdc1080;
+const uint32_t hdc1080_period_s = 10;
+void measure_hdc1080_sensor();
+Ticker hdc1080_measurement_timer(measure_hdc1080_sensor, hdc1080_period_s * 1e3,
+                                 0, MILLIS);
+#endif
+
 //// Post request
 WiFiClient client;
 HTTPClient http;
@@ -335,6 +346,75 @@ bool setup_ccs811_sensor() {
 
 #endif
 
+#ifdef HAS_HDC1080
+
+bool setup_hdc1080_sensor_json(JsonObject &sensor_json) {
+  sensor_json["name"] = sensor_hdc1080_name;
+  JsonArray sensor_1_in_magnitudes =
+      sensor_json.createNestedArray("magnitudes");
+
+  JsonObject mag1_in = sensor_1_in_magnitudes.createNestedObject();
+  mag1_in["name"] = "temperature";
+  mag1_in["unit"] = "C";
+  mag1_in["precision"] = 0.2;
+
+  JsonObject mag2_in = sensor_1_in_magnitudes.createNestedObject();
+  mag2_in["name"] = "humidity";
+  mag2_in["unit"] = "%";
+  mag2_in["precision"] = 2;
+
+  return true;
+}
+
+bool parse_hdc1080_sensor_json(JsonObject &sensor_json_response) {
+  hdc1080_sensor_id = sensor_json_response["id"];
+  JsonArray magnitudes_json =
+      sensor_json_response["magnitudes"].as<JsonArray>();
+  for (JsonObject mag_json : magnitudes_json) {
+    const char *name = mag_json["name"];
+    if (strcmp(name, "temperature") == 0) {
+      hdc1080_temp_id = mag_json["id"];
+    } else if (strcmp(name, "humidity") == 0) {
+      hdc1080_hum_id = mag_json["id"];
+    }
+  }
+  log_printf(
+      "  hdc1080_sensor_id: %d, hdc1080_temp_id: %d, hdc1080_hum_id: %d.\n",
+      hdc1080_sensor_id, hdc1080_temp_id, hdc1080_hum_id);
+  log_header_printf(
+      web_debug_info_header,
+      "HDC1080 sensor_id: %d, temperature_id: %d, humidity_id: %d.",
+      hdc1080_sensor_id, hdc1080_temp_id, hdc1080_hum_id);
+
+  return true;
+}
+
+bool setup_hdc1080_sensor() {
+  log_printf("Setting up HDC1080 sensor...\n");
+
+  // Enable HDC1080
+  hdc1080.begin(0x40);
+  hdc1080.setResolution(HDC1080_RESOLUTION_14BIT, HDC1080_RESOLUTION_14BIT);
+
+  // Print HDC1080 versions
+  log_printf("  Manufacturer ID: 0x%X\n", hdc1080.readManufacturerId());
+  log_printf("  Device ID: 0x%X\n", hdc1080.readDeviceId());
+  HDC1080_SerialNumber sernum = hdc1080.readSerialNumber();
+  log_printf("  Serial number: %02X-%04X-%04X.\n", sernum.serialFirst,
+             sernum.serialMid, sernum.serialLast);
+
+  delay(500);
+  log_println("  Done!");
+  log_header_printf(web_debug_info_header,
+                    "HDC1080 manufacturer ID v. 0x%X, dev ID: 0x%X, "
+                    "Serial no.: %02X-%04X-%04X.",
+                    hdc1080.readManufacturerId(), hdc1080.readDeviceId(),
+                    sernum.serialFirst, sernum.serialMid, sernum.serialLast);
+  return true;
+}
+
+#endif
+
 bool setup_sensors() {
   if (WiFi.status() != WL_CONNECTED) {
     connect_to_wifi();
@@ -423,6 +503,11 @@ bool setup_sensors() {
       parse_ccs811_sensor_json(sensor_json);
     }
 #endif
+#ifdef HAS_HDC1080
+    if (strcmp(name, sensor_hdc1080_name) == 0) {
+      parse_hdc1080_sensor_json(sensor_json);
+    }
+#endif
   }
 
   return true;
@@ -435,6 +520,10 @@ bool setup_internal_sensors() {
 
 #ifdef HAS_CCS811
   res = res && setup_ccs811_sensor();
+#endif
+
+#ifdef HAS_HDC1080
+  res = res && setup_hdc1080_sensor();
 #endif
 
   return res;
@@ -577,6 +666,61 @@ void measure_ccs811_sensor() {
 }
 #endif
 
+#ifdef HAS_HDC1080
+void measure_hdc1080_sensor() {
+  log_println("Measuring HDC1080...");
+  int conversion_ret_val;
+  time_t now = UTC.now();
+  float temperature, humidity;
+
+  // sensor
+  humidity = hdc1080.readHumidity();
+  temperature = hdc1080.readTemperature();
+
+  if (isnan(humidity)) {
+    log_println("  Error reading humidity.");
+    num_measurement_errors++;
+  } else {
+    SensorData humidity_data = {now, hdc1080_sensor_id, hdc1080_hum_id};
+    conversion_ret_val = snprintf(
+        humidity_data.value, sizeof(humidity_data.value), "%.3f", humidity);
+    if (conversion_ret_val > 0) {
+      log_printf("  Humidity: %.2f %%.\n", humidity);
+      sensor_buffer.push(humidity_data);
+      if (num_measurement_errors > 0) {
+        num_measurement_errors--;
+      }
+    } else {
+      log_printf("  Problem converting humidity (%.2f %%) to char[].\n",
+                 humidity);
+    }
+  }
+
+  if (isnan(temperature)) {
+    log_println("  Error reading temperature.");
+    num_measurement_errors++;
+  } else if (temperature > 120) {
+    log_printf("  Error reading temperature (%.2f)\n.", temperature);
+    num_measurement_errors++;
+  } else {
+    SensorData temperature_data = {now, hdc1080_sensor_id, hdc1080_temp_id};
+    conversion_ret_val =
+        snprintf(temperature_data.value, sizeof(temperature_data.value), "%.3f",
+                 temperature);
+    if (conversion_ret_val > 0) {
+      log_printf("  Temperature: %.2f C.\n", temperature);
+      sensor_buffer.push(temperature_data);
+      if (num_measurement_errors > 0) {
+        num_measurement_errors--;
+      }
+    } else {
+      log_printf("  Problem converting temperature (%.2f C) to char[].\n",
+                 temperature);
+    }
+  }
+}
+#endif
+
 ////// Send data functions
 bool post_measurement(String &data, String endpoint) {
   // Post Data
@@ -702,6 +846,9 @@ void setup() {
 #ifdef HAS_CCS811
   ccs811_measurement_timer.start();
 #endif
+#ifdef HAS_HDC1080
+  hdc1080_measurement_timer.start();
+#endif
   send_timer.start();
 }
 
@@ -724,6 +871,9 @@ void loop() {
 #endif
 #ifdef HAS_CCS811
   ccs811_measurement_timer.update();
+#endif
+#ifdef HAS_HDC1080
+  hdc1080_measurement_timer.update();
 #endif
   send_timer.update();
 }
