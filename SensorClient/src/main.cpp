e807c2db253a1eeaba8e0fed930b4d0188d0efd1
Pedro Villanueva <pedvide@gmail.com>
2021-01-24 23:27:40 +0100

    setup over the air update OTA

--- SensorClient/src/main.cpp
+++ SensorClient/src/main.cpp
@@ -1,6 +1,7 @@
 
 #include "ESPAsyncTCP.h"
 #include "ESPAsyncWebServer.h"
+#include <ArduinoOTA.h>
 #include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
 
@@ -536,6 +537,33 @@ void connect_to_wifi() {
   WiFi.setAutoReconnect(true);
 }
 
+void setup_OTA() {
+  ArduinoOTA.onStart([]() { Serial.println("Start updating"); });
+
+  ArduinoOTA.onEnd([]() { Serial.println("\nEnd"); });
+
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR)
+      Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR)
+      Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR)
+      Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR)
+      Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR)
+      Serial.println("End Failed");
+  });
+
+  ArduinoOTA.begin();
+  log_header_printf("OTA enabled.");
+}
+
 void connect_to_time() {
   log_println(F("Connecting to time server"));
   setDebug(ezDebugLevel_t::INFO);
@@ -846,6 +874,8 @@ void setup() {
 
   connect_to_wifi();
 
+  setup_OTA();
+
   connect_to_time();
 
   setup_web_server();
@@ -906,6 +936,9 @@ void setup() {
 void loop() {
   // Update time if needed
   events();
+
+  // Deal with OTA
+  ArduinoOTA.handle();
 
   if (requested_restart) {
     delay(10);
