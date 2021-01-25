a856f6c911649d930d4b0c464ea0fec09d639903
Pedro Villanueva <pedvide@gmail.com>
2021-01-25 19:18:22 +0100

    add debug infor for OTA

--- SensorClient/src/main.cpp
+++ SensorClient/src/main.cpp
@@ -540,26 +540,35 @@ void connect_to_wifi() {
 }
 
 void setup_OTA() {
-  ArduinoOTA.onStart([]() { Serial.println("Start updating"); });
+  pinMode(LED_BUILTIN, OUTPUT);
 
-  ArduinoOTA.onEnd([]() { Serial.println("\nEnd"); });
+  ArduinoOTA.onStart([]() {
+    digitalWrite(LED_BUILTIN, HIGH);
+    log_println(F("Starting OTA update."));
+  });
+
+  ArduinoOTA.onEnd([]() {
+    digitalWrite(LED_BUILTIN, LOW);
+    log_println(F("Finish OTA update."));
+  });
 
   ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+    log_printf("OTA progress: %u%%.\n", (progress / (total / 100)));
   });
 
   ArduinoOTA.onError([](ota_error_t error) {
-    Serial.printf("Error[%u]: ", error);
+    log_printf("OTA Error (%u): ", error);
     if (error == OTA_AUTH_ERROR)
-      Serial.println("Auth Failed");
+      log_println(F("Auth Failed"));
     else if (error == OTA_BEGIN_ERROR)
-      Serial.println("Begin Failed");
+      log_println(F("Begin Failed"));
     else if (error == OTA_CONNECT_ERROR)
-      Serial.println("Connect Failed");
+      log_println(F("Connect Failed"));
     else if (error == OTA_RECEIVE_ERROR)
-      Serial.println("Receive Failed");
+      log_println(F("Receive Failed"));
     else if (error == OTA_END_ERROR)
-      Serial.println("End Failed");
+      log_println(F("End Failed"));
   });
 
   ArduinoOTA.begin();
