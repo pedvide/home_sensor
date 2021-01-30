675487a32244b5fc46ce97156a75e0f15c533322
Pedro Villanueva <pedvide@gmail.com>
2021-01-31 00:08:17 +0100

    add station cards

---
+++ web_client/src/components/Station.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="card">
+    <div class="content">
+      <h4>Station {{ station.id }}</h4>
+      <p>Located at {{ station.location }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Station",
+  props: {
+    station: {
+      default: () => "",
+      type: Object,
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.card {
+  /* Add shadows to create the "card" effect */
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
+}
+
+/* On mouse-over, add a deeper shadow */
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+}
+
+/* Add some padding inside the card content */
+.content {
+  padding: 2px 16px;
+}
+</style>
--- web_client/src/views/Stations.vue
+++ web_client/src/views/Stations.vue
@@ -1,17 +1,43 @@
 <template>
-  <article class="home">
-    <HelloWorld msg="Home" />
+  <article class="stations">
+    <ol id="stations-list">
+      <li v-for="s in stations" :key="s.id">
+        <Station :station="s" />
+      </li>
+    </ol>
   </article>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import fetchData from "@/utils/api_query";
+import Station from "@/components/Station.vue";
 
 export default {
   name: "Stations",
   components: {
-    HelloWorld,
+    Station,
+  },
+  data() {
+    return {
+      stations: [],
+    };
+  },
+  created() {
+    fetchData.call(this, "stations");
+    setInterval(fetchData.bind(this), 1000, "stations");
   },
 };
 </script>
+
+<style scoped>
+ol {
+  list-style-type: none;
+  display: inline-block;
+  text-align: left;
+}
+/* li {
+  display: inline-block;
+  margin: 0 10px;
+} */
+</style>
