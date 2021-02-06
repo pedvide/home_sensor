c6e9583b2bdd2ecee4d5a5ec67056d0fbc4808bb
Pedro Villanueva <pedvide@gmail.com>
2021-02-06 13:10:00 +0100

    show sensors under a station after click

--- web_client/src/components/Station.vue
+++ web_client/src/components/Station.vue
@@ -1,19 +1,40 @@
 <template>
-  <div class="card">
+  <div @click="showSensors = !showSensors" class="card">
     <div class="content">
       <h4>Station {{ station.id }}</h4>
       <p>Located at the {{ station.location }}</p>
+      <ol v-if="showSensors" class="list">
+        <li v-for="s in station.sensors" :key="s.id" class="list">
+          <Sensor :sensor="s" />
+        </li>
+      </ol>
     </div>
   </div>
 </template>
 
 <script>
+import Sensor from "@/components/Sensor.vue";
+
 export default {
   name: "Station",
+  components: {
+    Sensor,
+  },
   props: {
     station: {
       default: () => "",
       type: Object,
+    },
+  },
+  data() {
+    return {
+      showSensors: false,
+    };
+  },
+  methods: {
+    testFunction(event) {
+      console.log("test clicked");
+      console.log(event);
     },
   },
 };
