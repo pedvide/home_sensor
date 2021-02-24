955794cebce833ac4fe5b656b364b75f921b7845
Pedro Villanueva <pedvide@gmail.com>
2021-02-24 07:46:54 +0100

    correctly format magnitudes with integer precision

--- web_client/src/components/Measurement.vue
+++ web_client/src/components/Measurement.vue
@@ -12,6 +12,7 @@
       </h5>
       <p>
         {{ m.magnitude.name[0].toUpperCase() + m.magnitude.name.substring(1) }}
+        {{ formatMagnitude(m.value, m.magnitude.precision) }}
         {{ m.magnitude.unit === "C" ? "°C" : m.magnitude.unit }}
       </p>
     </div>
@@ -25,6 +26,17 @@ export default {
     m: {
       default: () => "",
       type: Object,
+    },
+  },
+  methods: {
+    formatMagnitude(value, precision) {
+      const orderOfMagnitude = Math.floor(Math.log10(precision));
+      if (orderOfMagnitude < 0) {
+        return parseFloat(value).toFixed(-orderOfMagnitude);
+      }
+      return parseFloat(
+        parseInt(value, 10).toPrecision(value.length - orderOfMagnitude)
+      );
     },
   },
 };
